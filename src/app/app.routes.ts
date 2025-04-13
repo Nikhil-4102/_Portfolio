4aebcead82b1e1d1acce95ad02bc40f4940cee7e
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-04-13 14:10:00 +0530

    home done- about started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('./components/header/header.component').then(m => m.HeaderComponent)
+    },
+    {
+        path: 'projects',
+        loadComponent: () => import('./components/projects/projects.component').then(m => m.ProjectsComponent)
+    },
+    {
+        path: 'about',  
+        loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent)
+
+    },
+    {
+        path:'articles',
+        loadComponent: () => import('./components/articles/articles.component').then(m => m.ArticlesComponent)
+    }
+   
+];
