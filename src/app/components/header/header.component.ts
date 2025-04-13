af33d99c46907419573746df147ba6a1a986690e
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-04-13 11:00:36 +0530

    nav and home done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-header',
-  imports:[FlexLayoutModule],
+  standalone: true,
+  imports:[FlexLayoutModule ,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  view: any = 'home';
 
+    handleView(view:any){
+      this.view = view;
+    }
 }
