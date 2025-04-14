8453655d74d4055004af506edd810019f4ae0e72
Nikhil-4102 <nikhilmalave04ofcl@gmail.com>
2025-04-14 20:35:22 +0530

    about and projects added Article page remaining

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent,],
+  imports: [RouterOutlet, HeaderComponent, AboutComponent, ProjectsComponent],
   template: '<router-outlet></router-outlet>',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
